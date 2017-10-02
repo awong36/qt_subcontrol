4986abc362e591cf47503a6976b6bc2b1e223d69
awong36 <adrian.wong@welbilt.com>
2017-10-02 13:38:06 -0400

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,29 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->pushButton->setProperty("pagematches", true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_tabWidget_currentChanged(int index)
+{
+// Set all buttons to false
+ui->pushButton->setProperty("pagematches", false);
+ui->pushButton_2->setProperty("pagematches", false);
+ui->pushButton_3->setProperty("pagematches", false);
+// Set one of the buttons to true
+if (index == 0)
+ui->pushButton->setProperty("pagematches", true);
+else if (index == 1)
+ui->pushButton_2->setProperty("pagematches", true);
+else
+ui->pushButton_3->setProperty("pagematches", true);
+// Update buttons style
+ui->pushButton->style()->polish(ui->pushButton);
+ui->pushButton_2->style()->polish(ui->pushButton_2);
+ui->pushButton_3->style()->polish(ui->pushButton_3);
 }
